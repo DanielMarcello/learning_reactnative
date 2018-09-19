c91203a4b65b58a2f452a45642f050aa87d5cdb6
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-09-19 14:06:17 -0700

    adds new component
    Shows how are those new components called

--- TestProject/App.js
+++ TestProject/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    var names = {
+      a: "A",
+      b: "B"
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>MAY!</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Text>Hello World</Text>
+        <Greeting name={names.b}></Greeting>
       </View>
     );
   }
